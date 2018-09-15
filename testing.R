5639bac18ccd849ec801d01de22439694d08edf1
mkln <peruzzi@gmail.com>
2018-09-15 04:21:04 +0200

    moved testing.r

--- testing.R
+++ testing.R
@@ -9,5 +9,5 @@ testmat <- rnorm(nr * p) %>% matrix(ncol=p)
 rbenchmark::benchmark(package2::col_sums(testmat),
           package2::col_sums_imported(testmat), 
           colSums(testmat),
-          #package1::col_sums(testmat),
+          package1::col_sums(testmat),
           replications=1000)
